ccb1e88cf0a37a75d7a32b5012263804d78ff430
Axel Scarlet <106916550+AxelScarlet@users.noreply.github.com>
2024-07-10 00:25:36 +0800

    Delete Fotokopi directory

--- Fotokopi/Staff_Page/addStaff.php
+++
@@ -1,5 +0,0 @@
-<?php include('partials/menu.php'); ?>
-<?php $user = 'Staff';?>
-<?php include('partials/registerForm.php'); ?>
-<?php include('partials/footer.php'); ?>
-<?php include('partials/registerProcessStaff.php'); ?>
