6239dd2c0f4caf484023552285526bf1043ebd0b
Axel Scarlet <106916550+AxelScarlet@users.noreply.github.com>
2024-07-10 04:45:56 +0800

    Delete Fotokopi directory

--- Fotokopi/Staff_Page/addCustomer.php
+++
@@ -1,5 +0,0 @@
-<?php include('partials/menu.php'); ?>
-<?php $user = 'Customer';?>
-<?php include('partials/registerForm.php'); ?>
-<?php include('partials/footer.php'); ?>
-<?php include('partials/registerProcessCustomer.php'); ?>
