93bc01f4cd6087711f0e36a580465032b502226e
Axel Scarlet <106916550+AxelScarlet@users.noreply.github.com>
2024-07-11 22:48:37 +0800

    Delete Fotokopi directory

--- Fotokopi/Staff_Page/addMenu.php
+++
@@ -1,5 +0,0 @@
-<?php include('partials/menu.php'); ?>
-<?php $user = 'Menu';?>
-<?php include('partials/menuForm.php'); ?>
-<?php include('partials/footer.php'); ?>
-<?php include('partials/addMenuProcess.php'); ?>
